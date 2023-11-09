4d02b8dc776f8ab4edaee2ef1f6e0077ce1300eb
Javlon1 <muhamedmijotot@gmail.com>
2023-11-09 18:41:44 +0300

    end home intro

--- src/Components/Page/Home/Intro/Intro.jsx
+++ src/Components/Page/Home/Intro/Intro.jsx
@@ -1,11 +1,38 @@
 import * as React from 'react'
 import './Intro.scss'
 
+import { Autoplay } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+import { sliderData } from '../../../../Data/Data';
+
 export default function Intro() {
     return (
         <section className='intro'>
-            <div className='container'>
-                qw
+            <div className="container">
+                <Swiper
+                    modules={[Autoplay]}
+                    spaceBetween={10}
+                    slidesPerView={2}
+                    // autoplay={{
+                    //   delay: 2500,
+                    //   disableOnInteraction: false,
+                    // }}
+                >
+                    {
+                        sliderData?.map((e) => (
+                            <SwiperSlide key={e.id}>
+                                <div className='container__swiper'>
+                                    <img src={e.img} alt="" />
+                                </div>
+                            </SwiperSlide>
+                        ))
+                    }
+                </Swiper>
             </div>
         </section>
     )
--- src/Data/Data.js
+++ src/Data/Data.js
@@ -1,3 +1,10 @@
+import slider1 from "../Assets/Img/slider1.png"
+import slider2 from "../Assets/Img/slider2.png"
+import slider3 from "../Assets/Img/slider1.png"
+import slider4 from "../Assets/Img/slider2.png"
+import slider5 from "../Assets/Img/slider1.png"
+import slider6 from "../Assets/Img/slider2.png"
+
 export const HeaderData = [
     {
         id: 1,
@@ -83,5 +90,32 @@ export const Lang = [
     {
         id: 3,
         lang: 'en'
+    },
+]
+
+export const sliderData = [ 
+    {
+        id:1,
+        img:slider1,
+    },
+    {
+        id:2,
+        img:slider2,
+    },
+    {
+        id:3,
+        img:slider3,
+    },
+    {
+        id:4,
+        img:slider4,
+    },
+    {
+        id:5,
+        img:slider5,
+    },
+    {
+        id:6,
+        img:slider6,
     },
 ]
