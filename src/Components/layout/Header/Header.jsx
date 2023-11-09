6ee5cfe8e263488dd1a32c77db5e38cbe5d15def
javlon1 <muhamedmijotot@gmail.com>
2023-11-09 13:14:07 +0300

    start project fib - end header

---
+++ src/Components/layout/Header/Header.jsx
@@ -0,0 +1,93 @@
+import * as React from 'react'
+import { Link } from 'react-router-dom';
+import './Header.scss'
+import { HeaderData } from '../../../Data/Data'
+import { Context } from '../../../Context/Context';
+// import logo from "../../../Assets/Img/logo."
+
+export default function Header() {
+
+    const { lan, menu, setMenu } = React.useContext(Context)
+    const [scrol, setScrol] = React.useState(false)
+    const [nav, setNav] = React.useState(false)
+    const offSet = 80;
+
+    const getTop = () => window.pageYOffset || document.documentElement.scrollTop;
+
+    window.addEventListener('scroll', () => {
+        if (getTop() > offSet) {
+            setScrol(true)
+        } else {
+            setScrol(false)
+        }
+    })
+
+    setMenu(HeaderData[menu - 1].id)
+
+    return (
+        <header className={scrol ? 'header' : 'header'}>
+            <div className="container">
+
+                <div className="container__top">
+                    <div className="container__top__item">
+                        <Link to='/'
+                            onClick={() => {
+                                sessionStorage.setItem('menu', 1)
+                            }}>
+                            <img className='logo' src='' alt="logo" />
+                        </Link>
+                        <ul className='list'>
+                            <li className='list__top'>
+                                <p>Доставка Еды</p>
+                                <span>Андижан</span>
+                            </li>
+                            <li className='list__item'>
+                                <p>время доставки</p>
+                                <i className="bi bi-dot"></i>
+                                <p>от 31 мин</p>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <div className='container__top__info'>
+                        <Link
+                            to={`tel:${905251243}`}
+                            target="_blank"
+                            className='container__top__info__number'
+                        >
+                            905251243
+                        </Link>
+                        <div className='container__top__info__btn'>
+                            <p>корзинка</p>
+                            <span>1</span>
+                        </div>
+                        <div onClick={() => setNav(!nav)} className={nav ? 'container__top__info__btnHam active-Ham' : 'container__top__info__btnHam'}></div>
+                    </div>
+                </div>
+
+                <nav className='container__nav'>
+                    <ul className={nav ? 'container__nav__list active-nav' : 'container__nav__list'}>
+                        {
+                            HeaderData?.map((e) => (
+                                <Link key={e.id} to={e.link}>
+                                    <li className={`${menu === e.id ? 'container__nav__list__item active' : 'container__nav__list__item'}`}
+                                        onClick={() => {
+                                            setMenu(e.id)
+                                            sessionStorage.setItem('menu', e.id)
+                                        }}>
+                                        {e[`nav_${lan}`]}
+                                    </li>
+                                </Link>
+                            ))
+                        }
+                    </ul>
+                    <div className='container__nav__btn'>
+                        <p>корзинка</p>
+                        <span>1</span>
+                    </div>
+                </nav>
+
+            </div>
+        </header>
+    )
+}
---
+++ src/Data/Data.js
@@ -0,0 +1,87 @@
+export const HeaderData = [
+    {
+        id: 1,
+        link: '/',
+        nav_uz: 'Home',
+        nav_ru: 'домой',
+        nav_en: 'Home'
+    },
+    {
+        id: 2,
+        link: '/',
+        nav_uz: 'Home',
+        nav_ru: 'домой',
+        nav_en: 'Home'
+    },
+    {
+        id: 3,
+        link: '/',
+        nav_uz: 'Home',
+        nav_ru: 'домой',
+        nav_en: 'Home'
+    },
+    {
+        id: 4,
+        link: '/',
+        nav_uz: 'Home',
+        nav_ru: 'домой',
+        nav_en: 'Home'
+    },
+    {
+        id: 5,
+        link: '/',
+        nav_uz: 'Home',
+        nav_ru: 'домой',
+        nav_en: 'Home'
+    },
+    {
+        id: 6,
+        link: '/',
+        nav_uz: 'Home',
+        nav_ru: 'домой',
+        nav_en: 'Home'
+    },
+    {
+        id: 7,
+        link: '/',
+        nav_uz: 'Home',
+        nav_ru: 'домой',
+        nav_en: 'Home'
+    },
+    {
+        id: 8,
+        link: '/',
+        nav_uz: 'Home',
+        nav_ru: 'домой',
+        nav_en: 'Home'
+    },
+    {
+        id: 9,
+        link: '/',
+        nav_uz: 'Home',
+        nav_ru: 'домой',
+        nav_en: 'Home'
+    },
+    {
+        id: 10,
+        link: '/',
+        nav_uz: 'Home',
+        nav_ru: 'домой',
+        nav_en: 'Home'
+    },
+]
+
+export const Lang = [
+    {
+        id: 1,
+        lang: 'uz'
+    },
+    {
+        id: 2,
+        lang: 'ru'
+    },
+    {
+        id: 3,
+        lang: 'en'
+    },
+]
