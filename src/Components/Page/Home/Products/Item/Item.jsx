85964ff07c74bed24536174dfddc32493a21e281
javlon1 <muhamedmijotot@gmail.com>
2023-11-13 12:30:18 +0300

    card

--- src/Components/Page/Home/Products/Item/Item.jsx
+++ src/Components/Page/Home/Products/Item/Item.jsx
@@ -58,13 +58,12 @@ export default function Item() {
                                     <b>hfuehuf hefueh eijfie efuuhfeu</b>
                                     <p>Lorem ipsum dolor sit amet consectetur</p>
                                 </div>
-                                <button className="btn-close btnM" data-bs-dismiss="modal" aria-label="Close"></button>
+                                <button type="button" className="btn-close btnM" data-bs-dismiss="modal" aria-label="Close"></button>
                             </div>
                             <div className="modal-body">
                                 <div className="product">
                                     <div className="product__img">
                                         <img src="" alt="" />
-                                        ss
                                     </div>
                                     <div className="product__item">
                                         hh
