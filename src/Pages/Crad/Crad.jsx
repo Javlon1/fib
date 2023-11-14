5192a9bb5a005c2a8db903901876f46d734300b2
Javlon1 <muhamedmijotot@gmail.com>
2023-11-14 21:58:52 +0300

    start card page

--- src/Pages/Crad/Crad.jsx
+++ src/Pages/Crad/Crad.jsx
@@ -1,7 +1,10 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { Link } from 'react-router-dom'
+import { Context } from '../../Context/Context'
 import "./Crad.scss"
 
 const Crad = () => {
+    const { setMenu } = useContext(Context)
     return (
         <section className='card'>
             <div className="container">
@@ -32,6 +35,38 @@ const Crad = () => {
 
                     </li>
                 </ul>
+                <div className='container__order'>
+                    <h5 className='container__order__title'>Добавить к заказу?</h5>
+                </div>
+                <div className='container__others'>
+                    <h5 className='container__others__title'>Соусы к бортикам и закускам</h5>
+                </div>
+                <div className="container__bottom">
+                    <div className="container__bottom__left">
+                        <Link to='/'
+                            onClick={() => {
+                                setMenu(1)
+                                sessionStorage.setItem('menu', 1)
+                            }}>
+                            <i className="bi bi-chevron-left"></i>
+                            <p>Вернуться в магазин</p>
+                        </Link>
+                    </div>
+                    <div className="container__bottom__right">
+                        <span className='container__bottom__right__tatalPrice'>
+                            <p>Сумма заказа:</p>
+                            <b>1 048 $</b>
+                        </span>
+                        <Link to='/'
+                            onClick={() => {
+                                setMenu(1)
+                                sessionStorage.setItem('menu', 1)
+                            }}>
+                            Оформить заказ
+                            <i className="bi bi-chevron-right"></i>
+                        </Link>
+                    </div>
+                </div>
             </div>
         </section>
     )
