f65fbce444dd2bbd6c238d9275dcbee29fe36a51
Javlon1 <muhamedmijotot@gmail.com>
2023-11-15 19:18:27 +0300

    card

--- src/Pages/Crad/Crad.jsx
+++ src/Pages/Crad/Crad.jsx
@@ -3,6 +3,15 @@ import { Link } from 'react-router-dom'
 import { Context } from '../../Context/Context'
 import "./Crad.scss"
 
+import { Autoplay } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+import { newsData } from '../../Data/Data';
+
 const Crad = () => {
     const { setMenu } = useContext(Context)
     return (
@@ -37,12 +46,99 @@ const Crad = () => {
                 </ul>
                 <div className='container__order'>
                     <h5 className='container__order__title'>Добавить к заказу?</h5>
+
+                    <ul className='container__order__list'>
+                        <Swiper
+                            modules={[Autoplay]}
+                            slidesPerView={1}
+                            spaceBetween={10}
+                            autoplay={{
+                                delay: 2500,
+                                disableOnInteraction: false,
+                            }}
+                            breakpoints={{
+                                320: {
+                                    slidesPerView: 2,
+                                    spaceBetween: 20
+                                },
+                                640: {
+                                    slidesPerView: 2,
+                                    spaceBetween: 40
+                                },
+                                768: {
+                                    slidesPerView: 3,
+                                    spaceBetween: 10,
+                                },
+                                1000: {
+                                    slidesPerView: 3,
+                                    spaceBetween: 10,
+                                },
+                            }}
+                        >
+                            {
+                                newsData?.map((e) => (
+                                    <SwiperSlide key={e.id}>
+                                        <li key={e.id} className='container__order__list__item'>
+                                            <img src={e.img} alt="" />
+                                            <div className="container__order__list__item__div">
+                                                <b>{e.name}</b>
+                                                <p>от {e.price} $</p>
+                                            </div>
+                                        </li>
+                                    </SwiperSlide>
+                                ))
+                            }
+                        </Swiper>
+                    </ul>
                 </div>
                 <div className='container__others'>
                     <h5 className='container__others__title'>Соусы к бортикам и закускам</h5>
+
+                    <ul className='container__others__list'>
+                        <Swiper
+                            modules={[Autoplay]}
+                            slidesPerView={2}
+                            spaceBetween={10}
+                            autoplay={{
+                                delay: 2500,
+                                disableOnInteraction: false,
+                            }}
+                            breakpoints={{
+                                380: {
+                                    slidesPerView: 3,
+                                    spaceBetween: 20
+                                },
+                                640: {
+                                    slidesPerView: 4,
+                                    spaceBetween: 40
+                                },
+                                768: {
+                                    slidesPerView: 5,
+                                    spaceBetween: 10,
+                                },
+                                1000: {
+                                    slidesPerView: 6,
+                                    spaceBetween: 10,
+                                },
+                            }}
+                        >
+                            {
+                                newsData?.map((e) => (
+                                    <SwiperSlide key={e.id}>
+                                        <li key={e.id} className='container__others__list__item'>
+                                            <img src={e.img} alt="" />
+                                            <b>{e.name}</b>
+                                            <p>от {e.price} $</p>
+                                        </li>
+                                    </SwiperSlide>
+                                ))
+                            }
+                        </Swiper>
+                    </ul>
                 </div>
                 <div className="container__bottom">
                     <div className="container__bottom__left">
+                        <div className='container__bottom__left__div'></div>
                         <Link to='/'
                             onClick={() => {
                                 setMenu(1)
